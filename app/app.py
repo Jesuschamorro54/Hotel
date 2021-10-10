da4fd270a1edd7c59e4eea430889987ca09091fb
Ricardo Gomez <ricardo_a_gomez@hotmail.com>
2021-10-09 20:46:17 -0500

    Inicial

---
+++ app/app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask  
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = os.urandom( 50 )
+    
+    from views import main
+
+    app.register_blueprint(main)
+        
+    return app 
---
+++ app/views.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, blueprints, request, flash, redirect, url_for
+
+main= blueprints.Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
+
+@main.route('/adm_seguridad/')
+def adm_seguridad():
+    return render_template('adm_seguridad.html')
