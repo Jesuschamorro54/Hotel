d9244a72c27e43a1c568156c7b4864981960d648
Ricardo Gomez <ricardo_a_gomez@hotmail.com>
2021-10-10 12:44:10 -0500

    Update Domingo 10

--- app/views.py
+++ app/views.py
@@ -6,6 +6,14 @@ main= blueprints.Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+@main.route('/usr_registro/')
+def registro():
+    return render_template('usr_registro.html')
+
+@main.route('/usr_login/')
+def login():
+    return render_template('usr_login.html')
+
 @main.route('/dashboard/')
 def dashboard():
     return render_template('dashboard.html')
@@ -13,3 +21,11 @@ def dashboard():
 @main.route('/adm_seguridad/')
 def adm_seguridad():
     return render_template('adm_seguridad.html')
+
+@main.route('/adm_habitaciones/')
+def adm_habitaciones():
+    return render_template('adm_habitaciones.html')
+
+@main.route('/adm_reservas/')
+def adm_reservas():
+    return render_template('adm_reservas.html')
