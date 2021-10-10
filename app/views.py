18d15510ce6c3c62e09bdf5c7786316aeaa0968c
Ricardo Gomez <ricardo_a_gomez@hotmail.com>
2021-10-10 16:32:16 -0500

    Update Login completo al dashboard

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, blueprints, request, flash, redirect, url_for
+from flask import Flask, render_template, blueprints, request, flash, redirect, url_for, jsonify
 
 main= blueprints.Blueprint('main', __name__)
 
@@ -6,13 +6,22 @@ main= blueprints.Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+@main.route('/usr_login/', methods=['GET', 'POST'])
+def login():
+    if(request.method == 'POST'):
+
+        user_name= request.form['user_name']
+        usrpassword = request.form['usrpassword']
+
+        if(user_name =='ricagome@mail.com' and usrpassword =='1234'):
+
+            return render_template('dashboard.html')
+
+    return render_template('usr_login.html')
+
 @main.route('/usr_registro/')
 def registro():
     return render_template('usr_registro.html')
-
-@main.route('/usr_login/')
-def login():
-    return render_template('usr_login.html')
 
 @main.route('/dashboard/')
 def dashboard():
